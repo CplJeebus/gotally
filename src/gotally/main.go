061b0350545fb2ac9e4504ca1b3e535823bc818f
CplJeebus <26419780+CplJeebus@users.noreply.github.com>
2020-09-23 00:03:18 +0100

    Sample code works

--- src/gotally/main.go
+++ src/gotally/main.go
@@ -87,7 +87,7 @@ func main() {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
-	sheet :=
+	// sheet := "Students"
 	// Prints the names and majors of students in a sample spreadsheet:
 	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
 	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
