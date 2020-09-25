bf64fb18d80c66595f7e66ea7f41b817d3f90520
CplJeebus <26419780+CplJeebus@users.noreply.github.com@>
2020-09-25 21:49:52 +0100

    Fighting the linter.

--- src/gotally/main.go
+++ src/gotally/main.go
@@ -21,10 +21,12 @@ func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
 	// time.
 	tokFile := "token.json"
 	tok, err := tokenFromFile(tokFile)
+
 	if err != nil {
 		tok = getTokenFromWeb(config)
 		saveToken(tokFile, tok)
 	}
+
 	return config.Client(ctx, tok)
 }
 
@@ -43,6 +45,7 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	if err != nil {
 		log.Fatalf("Unable to retrieve token from web: %v", err)
 	}
+
 	return tok
 }
 
@@ -53,8 +56,10 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 		return nil, err
 	}
 	defer f.Close()
+
 	tok := &oauth2.Token{}
 	err = json.NewDecoder(f).Decode(tok)
+
 	return tok, err
 }
 
@@ -62,16 +67,22 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 func saveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+
 	if err != nil {
 		log.Fatalf("Unable to cache oauth token: %v", err)
 	}
 	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+
+	err := json.NewEncoder(f).Encode(token)
+	if err != nil {
+		log.Fatalf("Unable to enocde token: %v", err)
+	}
 }
 
 func main() {
 	ctx := context.Background()
 	b, err := ioutil.ReadFile("credentials.json")
+
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
@@ -81,9 +92,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(ctx, config)
 
+	client := getClient(ctx, config)
 	srv, err := sheets.New(client)
+
 	if err != nil {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
@@ -97,5 +109,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Print(resp)
 }
